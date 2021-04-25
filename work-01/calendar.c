f213134e116fc966c82e9d466fdf72dced58a5fa
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2021-04-24 21:19:10 -0300

    feat: wip

--- work-01/calendar.c
+++ work-01/calendar.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 
-#define QTD_VARIABLES (2 * sizeof(int))
+#define QTD_VARIABLES (4 * sizeof(int))
 #define NAME_SIZE (10 * sizeof(char))
+#define PHONE_SIZE (12 * sizeof(char))
 
 #define OPTIONS (*(int*)(pBuffer))
-#define SEARCH_NAME ((char*)(pBuffer + QTD_VARIABLES))
+#define SORT_FIELD (*(int*)(pBuffer + sizeof(int)))
+#define SORT_ORDER (*(int*)(pBuffer + 2 * sizeof(int)))
+#define OFFSET (*(int*)(pBuffer + 3 * sizeof(int)))
+
+#define SORT_NO 1
+#define SORT_NAME 2
+#define SORT_AGE 3
+#define SORT_PHONE 4
+
+#define ORDER_ASC 1 
+#define ORDER_DESC 2
 
 struct person{
     char name[10];
@@ -78,19 +90,17 @@ void delete(Fila *fila, void *pBuffer) {
         scanf("%s",  (char*)(pBuffer + QTD_VARIABLES));
         getchar();
 
-        printf("Nome: %s \n", (char*)(pBuffer + QTD_VARIABLES));
-
         for (nodo = fila->pointerFirst; nodo != NULL; nodo = nodo->pointerNext) {
             if(strcmp(nodo->person.name, (char*)(pBuffer + QTD_VARIABLES)) == 0) { 
-                nodo->pointerNext->pointerPrevious = nodo->pointerPrevious;
-                nodo->pointerPrevious->pointerNext = nodo->pointerNext;
-
-                if (nodo->pointerNext == NULL) {
-                    fila->pointerLast = nodo->pointerPrevious;
-                }
-                if (nodo->pointerPrevious == NULL) {
+                if (nodo->pointerPrevious != NULL) nodo->pointerPrevious->pointerNext = nodo->pointerNext;
+                else {
                     fila->pointerFirst = nodo->pointerNext;
                 }
+                if (nodo->pointerNext != NULL)  nodo->pointerNext->pointerPrevious = nodo->pointerPrevious;
+                else {
+                    fila->pointerLast = nodo->pointerPrevious;
+                }
+                fila->count--;
             }
         }
 
@@ -98,6 +108,83 @@ void delete(Fila *fila, void *pBuffer) {
             printf("Nome: %s \n", nodo->person.name);
             printf("Idade: %d\n", nodo->person.age); 
             printf("Telefone: %s\n", nodo->person.phone);      
+        }
+
+        free(nodo);
+}
+
+void sort(Fila *fila, void *pBuffer) {      
+        Nodo *nodo = malloc(sizeof(Nodo)); 
+
+        OFFSET = 0;
+
+        printf("Deseja ordenar a agenda? \n1) Não\n2) Por nome\n3) Por idade\n4) Por telefone\n5) Sair\n");
+        scanf("%d", &SORT_FIELD);
+        getchar(); 
+
+        // printf("Como a agenda deve ser ordenada? \n1) Crescente\n2) Decrescente\n");
+        // scanf("%d", &SORT_ORDER);
+        // getchar();
+
+        if (SORT_FIELD != SORT_NO) { 
+            Nodo *auxNodo = malloc(sizeof(Nodo)); 
+            Nodo *swapNodo = malloc(sizeof(Nodo)); 
+
+            switch(SORT_FIELD) {
+                case SORT_NAME:
+                    OFFSET += offsetof(struct person, name);
+                    break;
+                case SORT_AGE:
+                    OFFSET += offsetof(struct person, age);
+                    break;
+                case SORT_PHONE:
+                    OFFSET += offsetof(struct person, phone);
+                    break;
+                default:
+                break;
+            }
+
+            for (nodo = fila->pointerFirst; nodo->pointerNext != NULL;nodo = nodo->pointerNext) {
+                for (auxNodo = nodo->pointerNext; auxNodo != NULL; auxNodo = auxNodo->pointerNext) {
+                    if (SORT_NAME == SORT_FIELD || SORT_PHONE == SORT_FIELD ) {
+                        if (strcmp((char *)((char *)auxNodo->pointerPrevious + OFFSET), (char *)((char *)auxNodo + OFFSET)) > 0) {
+                            if(fila->pointerFirst == auxNodo->pointerPrevious) {
+                                fila->pointerFirst = auxNodo;
+                            }
+                            if(fila->pointerLast == auxNodo){
+                                fila->pointerLast = auxNodo->pointerPrevious;
+                            } 
+
+                            swapNodo->pointerNext = auxNodo->pointerPrevious;
+                            swapNodo->pointerPrevious = auxNodo->pointerPrevious->pointerPrevious;
+
+                            auxNodo->pointerPrevious->pointerNext  = auxNodo->pointerNext; 
+                            auxNodo->pointerPrevious->pointerPrevious = auxNodo;
+
+                            auxNodo->pointerNext  = swapNodo->pointerNext; 
+                            auxNodo->pointerPrevious = swapNodo->pointerPrevious;
+
+                            auxNodo = swapNodo; 
+                            nodo = fila->pointerFirst; 
+                        }
+                    }
+                    else {
+                        if (SORT_AGE == SORT_FIELD) {                        
+                            if (((int *)((char *)nodo + OFFSET), (int *)((char *)auxNodo + OFFSET)) > 0) {
+                            }
+                        }
+                    }
+                }
+            }
+
+            free(auxNodo);
+            free(swapNodo);
+        }
+
+        for (nodo = fila->pointerFirst; nodo != NULL; nodo = nodo->pointerNext) {
+            printf("\nNome: %s \n", nodo->person.name);
+            printf("Idade: %d\n", nodo->person.age); 
+            printf("Telefone: %s\n\n", nodo->person.phone); 
         }
 
         free(nodo);
@@ -112,8 +199,10 @@ int main () {
 
     OPTIONS = 0;
 
+    printf("Agenda\nRegras:\n1- Nome até 10 caracteres\n2- Telefones DDD+Número\n");
+
     do {
-        // printf("1) Incluir\n2) Apagar\n3) Buscar\n4) Listar\n5) Sair\n");
+        printf("1) Incluir\n2) Apagar\n3) Buscar\n4) Listar/Ordenar\n5) Sair\n");
         scanf("%d", &OPTIONS);
         getchar();
 
@@ -133,17 +222,11 @@ int main () {
 
                 break;
             }
-    //         case 4: {
-    //             for(int i = 0; i < qtdPeople; i++) {
-    //                 printf("Nome: %s \n", (char*)(pBuffer + QTD_VARIABLES + (TOTAL_SIZE * i)));
-    //                 printf("Idade: %d\n", *(int*)(pBuffer + QTD_VARIABLES + NAME_SIZE + (TOTAL_SIZE * i))); 
-    //                 printf("Telefone: %s\n", (char*)(pBuffer + QTD_VARIABLES + NAME_SIZE + AGE_SIZE + (TOTAL_SIZE * i))); 
-    //             }
-    //             break;
-    //         }            
-    //         case 5: {
-    //             if(pBuffer != NULL) free(pBuffer);
-    //         }
+            case 4: {
+                sort(fila, pBuffer);
+
+                break;
+            }
             default:
                 break;
         }
